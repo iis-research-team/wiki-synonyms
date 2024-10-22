caff9e15df9a3531a48bf8f27ae78c0c6a777a2c
Bruches Elena <bruches@bk.ru>
2024-10-22 17:31:49 +0700

    add 4th batch of data

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='wiki_synonyms',
-    version='0.4.0',
+    version='0.5.0',
     description='Python package for Wiki synonyms',
     url='https://github.com/iis-research-team/wiki-synonyms',
     author='IIS Research Team',
     author_email='bruches@bk.ru',
     license='MIT',
     packages=find_packages(include=['wiki_synonyms', 'wiki_synonyms.*']),
-    data_files=[('wiki_synonyms.data', ['wiki_synonyms/data/synonyms_1.json', 'wiki_synonyms/data/synonyms_2.json', 'wiki_synonyms/data/synonyms_3.json'])],
+    data_files=[('wiki_synonyms.data', [
+        'wiki_synonyms/data/synonyms_1.json',
+        'wiki_synonyms/data/synonyms_2.json',
+        'wiki_synonyms/data/synonyms_3.json',
+        'wiki_synonyms/data/synonyms_4.json'
+    ])],
     include_package_data=True,
     install_requires=[
                       'numpy',
